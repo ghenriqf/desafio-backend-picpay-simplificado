923d51532d5b927aa5edec94cb9274aed59fecd7
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-17 16:02:27 -0300

    feat: initialize Spring Boot project with Maven setup and basic configuration

---
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/DesafioBackendPicpaySimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.ghenriqf.desafio_backend_picpay_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendPicpaySimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendPicpaySimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghenriqf/desafio_backend_picpay_simplificado/DesafioBackendPicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghenriqf.desafio_backend_picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendPicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
