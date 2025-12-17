bc5a45b311288dfccc7a3d895b42fec9372bf834
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-17 16:26:44 -0300

    refactor: rename TransferenciaRepository to TransacaoRepository for consistency

--- src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/repository/TransferenciaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ghenriqf.desafio_backend_picpay_simplificado.repository;
-
-import com.ghenriqf.desafio_backend_picpay_simplificado.domain.transferencia.Transferencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransferenciaRepository extends JpaRepository<Transferencia,Long> {
-}
