2c6d4a992f3faa55262b14025bd3d18fcb827544
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-18 16:49:03 -0300

    feat: implement TransacaoService with authorization flow

---
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/service/TransacaoService.java
@@ -0,0 +1,66 @@
+package com.ghenriqf.desafio_backend_picpay_simplificado.service;
+
+import com.ghenriqf.desafio_backend_picpay_simplificado.domain.transacao.Transacao;
+import com.ghenriqf.desafio_backend_picpay_simplificado.domain.usuario.Usuario;
+import com.ghenriqf.desafio_backend_picpay_simplificado.dto.TransacaoDTO;
+import com.ghenriqf.desafio_backend_picpay_simplificado.repository.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransacaoService {
+    private final TransacaoRepository transacaoRepository;
+    private final UsuarioService usuarioService;
+    private final RestTemplate restTemplate;
+
+    @Value("${authorize.service.url}")
+    private String authorizeServiceUrl;
+
+    public TransacaoService(TransacaoRepository transacaoRepository, UsuarioService usuarioService, RestTemplate restTemplate) {
+        this.transacaoRepository = transacaoRepository;
+        this.usuarioService = usuarioService;
+        this.restTemplate = restTemplate;
+    }
+
+    public void criarTransacao (TransacaoDTO transacaoDTO) throws Exception {
+        Usuario remetente = usuarioService.findUsuarioById(transacaoDTO.remetenteId());
+        Usuario destinatario = usuarioService.findUsuarioById(transacaoDTO.destinatarioId());
+
+        usuarioService.validarTransacao(remetente,transacaoDTO.valor());
+
+        boolean isAutorizado = autorizacaoTransacao(remetente,transacaoDTO.valor());
+
+        if (!isAutorizado) {
+            throw new Exception("Transação não autorizada.");
+        }
+
+        Transacao transacao = new Transacao();
+        transacao.setValor(transacaoDTO.valor());
+        transacao.setRemetente(remetente);
+        transacao.setDestinatario(destinatario);
+        transacao.setDataHora(LocalDateTime.now());
+
+        remetente.setSaldo(remetente.getSaldo().subtract(transacao.getValor()));
+        destinatario.setSaldo(destinatario.getSaldo().add(transacao.getValor()));
+
+        transacaoRepository.save(transacao);
+        usuarioService.salvarUsuario(remetente);
+        usuarioService.salvarUsuario(destinatario);
+    }
+
+    public boolean autorizacaoTransacao (Usuario usuario, BigDecimal valor) {
+        ResponseEntity<Map> autorizacaoResponse = restTemplate.getForEntity(authorizeServiceUrl, Map.class);
+
+        if (autorizacaoResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) autorizacaoResponse.getBody().get("message");
+            return "autorizado".equalsIgnoreCase(message);
+        } else return false;
+    }
+}
