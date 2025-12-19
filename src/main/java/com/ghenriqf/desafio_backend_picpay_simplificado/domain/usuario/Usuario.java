eaa6df627c187f47ec0083298191ac493872f0da
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-19 17:35:01 -0300

    refactor: remove constructor that couples user entity to DTO

--- src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/domain/usuario/Usuario.java
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/domain/usuario/Usuario.java
@@ -1,6 +1,5 @@
 package com.ghenriqf.desafio_backend_picpay_simplificado.domain.usuario;
 
-import com.ghenriqf.desafio_backend_picpay_simplificado.dto.UsuarioDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -31,13 +30,4 @@ public class Usuario {
     private BigDecimal saldo;
 
     private TipoDoUsuario tipo;
-
-    public Usuario(UsuarioDTO usuarioDTO) {
-        this.nomeCompleto = usuarioDTO.nomeCompleto();
-        this.cpf = usuarioDTO.cpf();
-        this.email = usuarioDTO.cpf();
-        this.saldo = usuarioDTO.saldo();
-        this.tipo = usuarioDTO.tipoDoUsuario();
-        this.senha = usuarioDTO.senha();
-    }
 }
