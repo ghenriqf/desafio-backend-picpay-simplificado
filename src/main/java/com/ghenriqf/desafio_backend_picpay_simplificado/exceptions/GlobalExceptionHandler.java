8b6cc1091aa3ca562fc39103c47332ddd0e2a239
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-19 15:53:58 -0300

    feat: implement global exception handling with custom exceptions

---
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.ghenriqf.desafio_backend_picpay_simplificado.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    public ResponseEntity<ExceptionResponse> handleUsuarioNaoEncontrado(UsuarioNaoEncontradoException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(),HttpStatus.NOT_FOUND.value() ,LocalDateTime.now());
+
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(AutorizacaoException.class)
+    public ResponseEntity<ExceptionResponse> handleAutorizacao(AutorizacaoException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatus.FORBIDDEN.value(), LocalDateTime.now());
+
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(ServicoExternoException.class)
+    public ResponseEntity<ExceptionResponse> handleServico(ServicoExternoException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatus.SERVICE_UNAVAILABLE.value(), LocalDateTime.now());
+
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+}
