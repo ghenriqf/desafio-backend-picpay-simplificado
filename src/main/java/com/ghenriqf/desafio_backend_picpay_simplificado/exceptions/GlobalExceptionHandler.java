e996539f602912a72b7e562317d9eec0d105e2f5
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-19 16:03:05 -0300

    feat: handle SaldoInsuficienteException in global exception handler

--- src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ghenriqf.desafio_backend_picpay_simplificado.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServicoExternoException.class)
     public ResponseEntity<ExceptionResponse> handleServico(ServicoExternoException exception) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatus.SERVICE_UNAVAILABLE.value(), LocalDateTime.now());
+
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(SaldoInsuficienteException.class)
+    public ResponseEntity<ExceptionResponse> handleSaldoInsuficiente(SaldoInsuficienteException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatusCode.valueOf(422).value(), LocalDateTime.now());
 
         return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
     }
