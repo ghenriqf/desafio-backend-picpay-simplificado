ae960f0ce1e1da8fe4366f409643ca16eb9ecf7d
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-22 17:45:11 -0300

    fix: handle data integrity violation exception

--- src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ghenriqf.desafio_backend_picpay_simplificado.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +36,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SaldoInsuficienteException.class)
     public ResponseEntity<ExceptionResponse> handleSaldoInsuficiente(SaldoInsuficienteException exception) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatusCode.valueOf(422).value(), LocalDateTime.now());
+
+        return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleDataIntegrityViolation(DataIntegrityViolationException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("Usuário já cadastrado", HttpStatus.BAD_REQUEST.value(), LocalDateTime.now());
 
         return ResponseEntity.status(exceptionResponse.getStatus()).body(exceptionResponse);
     }
