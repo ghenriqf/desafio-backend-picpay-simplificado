ddd0a84388db0d6054f7eb0f608bf334c6f985e0
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-12-22 17:43:24 -0300

    refactor: rename TransacaoDTO to TransacaoRequest

--- src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/dto/TransacaoRequest.java
+++ src/main/java/com/ghenriqf/desafio_backend_picpay_simplificado/dto/TransacaoRequest.java
@@ -2,7 +2,7 @@ package com.ghenriqf.desafio_backend_picpay_simplificado.dto;
 
 import java.math.BigDecimal;
 
-public record TransacaoDTO(
+public record TransacaoRequest(
         BigDecimal valor,
         Long remetenteId,
         Long destinatarioId
